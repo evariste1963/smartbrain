524160dea530a0046dc67c9885204c19a705f2d9
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-05-17 10:28:40 +0100

    using box-shadow inplace of tachyons shadow-5 --> index.css

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,13 +4,13 @@ import "./ImageLinkForm.css";
 const ImageLinkForm = () => {
   return (
     <div>
-      <p className="f3">
+      <p className='f3'>
         {"This magic Brain will detect faces in your pictures. Give it a try"}
       </p>
-      <div className="center">
-        <div className=" form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="tex" />
-          <button className="w-30 grow f4 link ph3 pv2 white bg-light-purple">
+      <div className='center'>
+        <div className=' form center pa4 br3 shadow'>
+          <input className='f4 pa2 w-70 center' type='tex' />
+          <button className='w-30 grow f4 link ph3 pv2 white bg-light-purple'>
             Detect
           </button>
         </div>
