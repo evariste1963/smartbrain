de98a9792c781ed5ccc43b228fe67baf6efb7dd2
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-05-27 08:34:48 +0100

    parallax adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,20 +5,20 @@ import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma1 mt0 ml5">
+    <div className='ma1 mt0 ml5'>
       <Tilt
-        className="Tilt br-80
-        shadow"
-        options={{ max: 55 }}
+        className='Tilt shadow'
+        tiltMaxAngleX={30}
+        tiltMaxAngleY={30}
         style={{ height: 180, width: 180 }}
       >
-        <div className="Tilt-inner pa3">
+        <div className='Tilt-inner pa3'>
           <img
             style={{ paddingTop: "15px", height: 95, width: 95 }}
-            alt="logo"
+            alt='logo'
             src={Brain}
           />
-          <h3 className="tag">this.me</h3>
+          <h3 className='tag'>this.me</h3>
         </div>
       </Tilt>
     </div>
