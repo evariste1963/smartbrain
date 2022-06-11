6a9f9115b1ed72ada2fa26524961d26c382ee2ca
evariste1963 <evariste@hotmail.co.uk>
2022-06-11 17:03:08 +0100

    signin working woth backend

--- src/App.js
+++ src/App.js
@@ -37,14 +37,14 @@ class App extends Component {
     };
   }
 
-  calculateFaceLocation = response => {
+  calculateFaceLocation = (response) => {
     const clarifaiFaces = response.outputs[0].data.regions.map(
-      region => region.region_info.bounding_box
+      (region) => region.region_info.bounding_box
     ); //returns an array of objects -- bounding_box %'s
     const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return clarifaiFaces.map(face => {
+    return clarifaiFaces.map((face) => {
       return {
         leftCol: face.left_col * width,
         topRow: face.top_row * height,
@@ -54,11 +54,11 @@ class App extends Component {
     });
   };
 
-  displayFaceBox = boxes => {
+  displayFaceBox = (boxes) => {
     this.setState({ boxes: boxes }); //boxes = returned array of boxes above -- sets state.boxes
   };
 
-  onInputChange = event => {
+  onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
 
@@ -66,14 +66,14 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
+      .then((response) =>
         this.displayFaceBox(this.calculateFaceLocation(response))
       )
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
     document.querySelector("input").value = "";
   };
 
-  onRouteChange = route => {
+  onRouteChange = (route) => {
     if (route === "signout") {
       this.setState({ isSignedIn: false });
     } else if (route === "home") {
@@ -85,7 +85,7 @@ class App extends Component {
   render() {
     const { isSignedIn, route, boxes, imageUrl } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <Particle />
         <Navigation
           isSignedIn={isSignedIn}
@@ -94,7 +94,7 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <div>
-              <Logo className='inline' />
+              <Logo className="inline" />
               <Rank />
             </div>
             <ImageLinkForm
