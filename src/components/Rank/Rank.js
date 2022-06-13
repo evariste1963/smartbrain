e65d5ff1f68d126490c0047f1dd3fbc42c80295e
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-06-13 15:27:24 +0100

    slight cleanup

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({ name, entries }) => {
   return (
     <div className='w-100'>
       <div className='f3 white center pt4 '>
-        {`${name}, your current entry count is...`}
+        {`${name}, your current image count is...`}
       </div>
       <div className='f1 white'>{entries}</div>
     </div>
