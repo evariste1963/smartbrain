c8560c82999c1ff1f3bf9399fc6d8c3706cd4771
evariste1963 <evariste@hotmail.co.uk>
2022-06-17 20:51:26 +0100

    helper working -- refactored

--- src/components/Helper/Helper.js
+++ src/components/Helper/Helper.js
@@ -1,5 +1,5 @@
-const Helper = (url, email, password, name) => {
-  return fetch(`http://localhost:3000/${url}`, {
+const Helper = (path, email, password, name) => {
+  return fetch(`http://localhost:3000/${path}`, {
     method: "post",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify({
@@ -7,7 +7,7 @@ const Helper = (url, email, password, name) => {
       password: password,
       name: name,
     }),
-  }).then(response => response.json());
+  }).then((response) => response.json());
 };
 
 export default Helper;
